81cd671dc7d5410d54131fce1bf409474c030857
Marcin Wisnicki <mwisnicki@gmail.com>
2022-06-20 07:58:13 -0400

    Fix loading of secrets from localStorage

--- src/secrets.js
+++ src/secrets.js
@@ -1,7 +1,15 @@
 const secrets = ["GAPI_API_KEY", "GAPI_CLIENT_ID"];
 
-const secret = (x) =>
-  localStorage[x] ?? (localStorage[x] = prompt(`Provide ${x}`));
+const secret = (key) => {
+  let value = localStorage[key];
+  if (!value) {
+    value = prompt(`Provide ${key}`);
+    if (value) {
+      localStorage[key] = value;
+    }
+  }
+  return value;
+};
 
 // always import whole object, directly importing keys will return nulls
 export default Object.fromEntries(secrets.map((key) => [key, secret(key)]));
